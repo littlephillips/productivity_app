83faeccdb7d421ea1bb79005a8935585ab8e95ec
littlephillips <phillipkinuthia72@gmail.com>
2025-12-01 21:04:24 +0300

    updated the React App

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
-import AuthForm from "./components/AuthForm";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+// // import AuthForm from "./components/AuthForm";
+// import Header from "./components/Header";
+
 
 function App() {
-  const user = JSON.parse(localStorage.getItem("user"));
+  // const user = JSON.parse(localStorage.getItem("user"));
 
   return (
     <Router>
       <div className="min-h-screen bg-gray-900 flex flex-col">
-        <Header />
+        {/* <Header /> */}
         <main className="flex-grow">
-          <Routes>
+          {/* <Routes>
             <Route path="/" element={user ? <Dashboard /> : <Navigate to="/login" />} />
             <Route path="/login" element={<AuthForm type="Login" />} />
             <Route path="/signup" element={<AuthForm type="Signup" />} />
-          </Routes>
+          </Routes> */}
+
+             <Routes>
+            <Route path="/" element={ <Dashboard />} />
+          </Routes> 
         </main>
       </div>
     </Router>
